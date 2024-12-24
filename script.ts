af85ee7f028faae1392dae101322188927608318
TatyanaZakiryanova <elenastrell@yandex.ru>
2024-12-24 21:05:47 +0300

    render method

--- script.ts
+++ script.ts
@@ -27,13 +27,28 @@ class TodoApp {
   public addTodo(task: string): void {
     const newTodo: ITodo = new Todo(this.todos.length + 1, task);
     this.todos.push(newTodo);
+    this.render();
   }
 
   public removeTodo(id: number): void {
     this.todos = this.todos.filter((todo) => todo.id !== id);
+    this.render();
   }
 
   public markAllCompleted(): void {
     this.todos.forEach((todo) => todo.markCompleted());
+    this.render();
+  }
+
+  private render(): void {
+    const todoListContainer = document.getElementById('todo-list');
+    if (todoListContainer) {
+      todoListContainer.innerHTML = '';
+      this.todos.forEach((todo) => {
+        const todoElement = document.createElement('li');
+        todoElement.textContent = todo.task;
+        todoListContainer.appendChild(todoElement);
+      });
+    }
   }
 }
